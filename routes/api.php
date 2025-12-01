dffcd9bff11942fe15da2d3ad83fbfd04767aa54
ali-mdaghmesh <alimdaghmesh116@gmail.com>
2025-12-01 14:01:20 +0300

    testing

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/status', function () {
+    return response()->json(['status' => 'API is running']);
+});
