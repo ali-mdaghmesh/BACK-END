59af6ca56cb87d52f899ce2911a7c375bfb2ea44
ali-mdaghmesh <alimdaghmesh116@gmail.com>
2025-12-01 14:22:51 +0300

    changings

--- database/migrations/2025_12_01_110749_create_profiles_table.php
+++ database/migrations/2025_12_01_110749_create_profiles_table.php
@@ -17,7 +17,8 @@ return new class extends Migration
             $table->string('first_name');
             $table->string('last_name');
             $table->string('phone_number'); 
-            $table->string('image_url'); 
+            $table->string('profile_picture_url');
+            $table->string('identity_image_url');
             $table->string('date_of_birth');
         });
     }
