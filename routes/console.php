e0c4bc2ebceca52ff1c2f4b895e03b24f7d54f57
ali-mdaghmesh <alimdaghmesh116@gmail.com>
2025-12-16 23:10:31 +0300

    add scheduling to handle (expired,done) reservations

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,29 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
+use Illuminate\Support\Facades\DB;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
+Artisan::command('reservations:update-ended', function () {
+
+    $done = DB::table('reservations')
+        ->where('end_date', '<', Carbon::today())
+        ->where('status', 'approved')
+        ->update(['status' => 'done']);
+
+        $expired = DB::table('reservations')
+        ->where('status', '!=', 'approved')           
+        ->where('status', '!=', 'done')              
+        ->where('status', '!=', 'expired')   
+        ->where('status', '!=', 'rejected')       
+        ->where('status', '!=', 'cancelled')
+        ->where('end_date', '<', Carbon::today())
+        ->update(['status' => 'expired']);
+
+    $this->info("successfully updated $done reservations to done status");
+    $this->info("successfully updated $expired reservations to expired status");
+});
+
+
+Schedule::command('reservations:update-ended')->everyMinute();
